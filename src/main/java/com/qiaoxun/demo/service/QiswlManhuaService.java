fb683350f09aa5f582815a5861b7db736bd09d1a
LHB <2390257098@qq.com>
2019-05-17 14:57:10 +0800

    。。。

--- src/main/java/com/qiaoxun/demo/service/QiswlManhuaService.java
+++ src/main/java/com/qiaoxun/demo/service/QiswlManhuaService.java
@@ -5,7 +5,6 @@ import com.qiaoxun.demo.pojo.QiswlManhua;
 import java.io.IOException;
 
 public interface QiswlManhuaService {
-    int insertSelective(QiswlManhua record);
 
     void parse() throws IOException;
 
--- src/main/java/com/qiaoxun/demo/service/impl/QiswlManhuaServiceImpl.java
+++ src/main/java/com/qiaoxun/demo/service/impl/QiswlManhuaServiceImpl.java
@@ -1,14 +1,11 @@
 package com.qiaoxun.demo.service.impl;
 
-import com.qiaoxun.demo.dao.QiswlCapterDao;
 import com.qiaoxun.demo.dao.QiswlManhuaDao;
 import com.qiaoxun.demo.pojo.QiswlCapterWithBLOBs;
 import com.qiaoxun.demo.pojo.QiswlManhua;
 import com.qiaoxun.demo.service.QiswlManhuaService;
-import com.qiaoxun.util.DownloadImg;
 import com.qiaoxun.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -39,10 +36,10 @@ public class QiswlManhuaServiceImpl implements QiswlManhuaService {
 
     CommonMethodsImpl methods = new CommonMethodsImpl();
 
-    @Override
-    public int insertSelective(QiswlManhua record) {
-        return manhuaDao.insertSelective(record);
-    }
+    String init="http://www.yymh8.com";
+
+    Date date=new Date();
+
     /**
      * 爬取数据  下载图片  数据入库
      */
@@ -52,20 +49,15 @@ public class QiswlManhuaServiceImpl implements QiswlManhuaService {
         String initUrl = "http://www.yymh8.com/index.php?m=&c=Mh&a=book_cate&p_reload=1&reload_time=1557799064903";
         // 直接获取DOM树，带着cookies去获取
         document = Jsoup.connect(initUrl).cookies(cookies).timeout(1000*60*2).post();
-
-        String init="http://www.yymh8.com";
         //选择器定位
         Elements elements = document.select("#html_box").select(".item");
         cartoons=elements.size();
-
         //创建时间和更新时间
-        Date date=new Date();
+
         qiswlManhua.setCreateTime(date);
         qiswlManhua.setUpdateTime(date);
-        //入库操作
-        SqlSession sqlSession=MyBatisUtil.createSqlSession();
-        //获取内部元素（目标数据）
 
+        //获取内部元素（目标数据）
         //i表示漫画的顺序
         for (int i=1;i<=elements.size();i++) {
             System.out.println("这是第"+i+"个漫画");
@@ -160,50 +152,109 @@ public class QiswlManhuaServiceImpl implements QiswlManhuaService {
         // 直接获取DOM树，带着cookies去获取
         document = Jsoup.connect(initUrl).cookies(cookies).post();
         Elements elements = document.select("#html_box").select(".item");
+        //结婚接收漫画名称
+        List<String> titles=manhuaDao.selectTitles() ;
         cartoons=elements.size();
         for (int i=1;i<=cartoons;i++) {
             String url = "http://www.yymh8.com"+elements.get(i-1).select("a").attr("href").trim();
             document = Jsoup.connect(url).cookies(cookies).get();
             String title=document.select("body > div.cover-box > div.container > div.title").text();
-            String lastChapterTitle=document.select("#chapters > div.ch > div > span").text();
-            System.out.println(lastChapterTitle+"-------------");
-            String lastChapter="第"+lastChapterTitle.substring(3);
-            String last=manhuaDao.selectLastChapterByTitle(title);
-            System.out.println(last);
-            if(last.equals(lastChapter)){
-                System.out.println("当前已是最新章节，请耐心等待");
+            //判断是否有新漫画
+            //false
+            if (titles.toString().substring(1,titles.toString().length()-1).contains(title)){
+                String status=document.select("#chapters > div.ch > div").text().substring(0,2);
+                //判断状态
+                if (status.equals("已完结")){
+                    //如果true------修改漫画状态
+                    manhuaDao.updateStatusByTitle(title);
+                }else {
+                    String lastChapterTitle=document.select("#chapters > div.ch > div > span").text();
+                    System.out.println(lastChapterTitle+"-------------");
+                    String lastChapter="第"+lastChapterTitle.substring(3);
+                    String last=manhuaDao.selectLastChapterByTitle(title);
+                    if(last.equals(lastChapter)){
+                        System.out.println("当前已是最新章节，请耐心等待");
+                    }else {
+                        System.out.println("正在尽力补全，请耐心等待");
+                        String chapterUrl=elements.get(i-1).select("#html_box > div:nth-child(1) > a").attr("href").trim();
+
+                        //更新漫画表里的最新章节字段
+                        manhuaDao.updateLastChapter(title,lastChapter);
+
+                        int htmlLastChapter=Integer.parseInt(lastChapterTitle.substring(3,lastChapterTitle.length()-1));
+                        for (int sqlLastChapter=Integer.parseInt(last.substring(1,last.length()-1));sqlLastChapter<htmlLastChapter;sqlLastChapter++){
+                            document = Jsoup.connect(chapterUrl.substring(0,chapterUrl.length()-1)+sqlLastChapter).cookies(cookies).get();
+                            Elements elements2=document.select(".read-article").select(".item");
+                            System.out.println("第"+(sqlLastChapter+1)+"话里边有"+elements2.size()+"张图片");
+                            System.out.println("开始下载图片了！！！");
+                            //j代表第q个章节里的第j张图片
+                            for (int j=1;j<=elements2.size();j++){
+                                System.out.println("正在下载第"+j+"张图片ing...");
+                                //获取章节内部每张图的url
+                                String imgUrl=elements2.get(j-1).select("img").attr("src").trim();
+                                //下载每个章节里的图片
+                                imgUrlSql=methods.download3(imgUrl,i,sqlLastChapter+1,j);
+                                //加进集合------入库后清空集合
+                                imgUrlSqls.add(imgUrlSql);
+                            }
+                            bloBs.setImagelist(imgUrlSqls.toString().substring(1,imgUrlSqls.toString().length()-1));
+                            bloBs.setManhuaId(qiswlManhua.getId());
+                            bloBs.setCreateTime(new Date());
+                            bloBs.setUpdateTime(new Date());
+                            bloBs.setCjid(i+"");
+                            bloBs.setSort(sqlLastChapter+1);
+                            bloBs.setTitle("第"+(sqlLastChapter+1)+"话");
+                            chapterService.insertSelective(bloBs);
+                            imgUrlSqls.clear();
+                        }
+                    }
+                }
             }else {
-                System.out.println("正在尽力补全，请耐心等待");
-                String chapterUrl=elements.get(i-1).select("#html_box > div:nth-child(1) > a").attr("href").trim();
-                //数据库内信息
-                //int sql=Integer.parseInt(last.substring(1,last.length()-1));
-                //更新数据库
-
-                manhuaDao.updateLastChapter(title,lastChapter);
-
-                int html=Integer.parseInt(lastChapterTitle.substring(3,lastChapterTitle.length()-1));
-                for (int sql=Integer.parseInt(last.substring(1,last.length()-1));sql<html;sql++){
-                    document = Jsoup.connect(chapterUrl.substring(0,chapterUrl.length()-1)+sql).cookies(cookies).get();
+                //以下代码是重复代码------不加注释了
+                System.out.println("这是第"+i+"个漫画");
+                String image=elements.get(i-1).select("a > div.cover > img").attr("src").trim();
+                qiswlManhua.setImage(methods.download1(image,i));
+                document = Jsoup.connect(url).cookies(cookies).get();
+                qiswlManhua.setTitle(title);
+                String cover=document.select("body > div.cover-box > div.bg > img").attr("src").trim();
+                String newCover=methods.download2(cover,i);
+                qiswlManhua.setCover(newCover);
+                String desc=document.select("#book-info > article > div.body").text();
+                qiswlManhua.setDesc(desc.substring(0,desc.length()-10));
+                String author=document.select("#book-info > article > div.author").text();
+                qiswlManhua.setAuther(author.substring(3));
+                String keyWord=document.select("body > div.cover-box > div.container > div.tags").text();
+                qiswlManhua.setKeyword(keyWord);
+                String view=document.select("#book-info > div > div:nth-child(1)").text();
+                qiswlManhua.setView(Integer.parseInt(view.substring(4)));
+                String mark=document.select("#book-info > div > div:nth-child(3)").text();
+                qiswlManhua.setMark(Integer.parseInt(mark.substring(0, mark.indexOf("人"))));
+                String lastChapterTitle=document.select("#chapters > div.ch > div > span").text();
+                String chapterUrl=document.select("#html_box > div:nth-child(1) > a").attr("href").trim();
+                String chapter=init+chapterUrl.substring(0,chapterUrl.length()-1);
+                int chapters=Integer.parseInt(lastChapterTitle.substring(3,lastChapterTitle.length()-1));
+                qiswlManhua.setLastChapter("第"+chapters+"话");
+                qiswlManhua.setLastChapterTitle("第"+chapters+"话");
+                manhuaDao.insertSelective(qiswlManhua);
+                for (int q=1;q<=chapters;q++){
+                    String newUrl=chapter+q;
+                    document = Jsoup.connect(newUrl).cookies(cookies).timeout(1000*60*2).get();
                     Elements elements2=document.select(".read-article").select(".item");
-                    System.out.println("第"+(sql+1)+"话里边有"+elements2.size()+"张图片");
+                    System.out.println("第"+q+"话里边有"+elements2.size()+"张图片");
                     System.out.println("开始下载图片了！！！");
-                    //j代表第q个章节里的第j张图片
                     for (int j=1;j<=elements2.size();j++){
                         System.out.println("正在下载第"+j+"张图片ing...");
-                        //获取章节内部每张图的url
                         String imgUrl=elements2.get(j-1).select("img").attr("src").trim();
-                        //下载每个章节里的图片
-                        imgUrlSql=methods.download3(imgUrl,i,sql+1,j);
-                        //加进集合------入库后清空集合
+                        String imgUrlSql=methods.download3(imgUrl,i,q,j);
                         imgUrlSqls.add(imgUrlSql);
                     }
-                    bloBs.setImagelist(imgUrlSqls.toString().substring(1,imgUrlSqls.toString().length()-1));
-                    bloBs.setManhuaId(i);
-                    bloBs.setCreateTime(new Date());
-                    bloBs.setUpdateTime(new Date());
+                    bloBs.setTitle("第"+q+"话");
                     bloBs.setCjid(i+"");
-                    bloBs.setSort(sql+1);
-                    bloBs.setTitle("第"+(sql+1)+"话");
+                    bloBs.setUpdateTime(date);
+                    bloBs.setCreateTime(date);
+                    bloBs.setImagelist(imgUrlSqls.toString());
+                    bloBs.setManhuaId(qiswlManhua.getId());
+                    bloBs.setSort(q);
                     chapterService.insertSelective(bloBs);
                     imgUrlSqls.clear();
                 }
