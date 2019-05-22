6d236b640ae00effffc1b65d83954d778243d4a8
LHB <2390257098@qq.com>
2019-05-22 08:57:19 +0800

    pojo

---
+++ src/main/java/com/qiaoxun/demo/pojo/MeituanShopInfo.java
@@ -0,0 +1,155 @@
+package com.qiaoxun.demo.pojo;
+
+
+public class MeituanShopInfo {
+
+  private long id;
+  private String name;
+  private String phone;
+  private String city;
+  private String address;
+  private java.sql.Timestamp crawlerDate;
+  private String orderNum;
+  private String avgTime;
+  private String avgCost;
+  private String method;
+  private String phone2;
+  private long isNew;
+  private String type;
+  private String score;
+  private String distance;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+
+  public java.sql.Timestamp getCrawlerDate() {
+    return crawlerDate;
+  }
+
+  public void setCrawlerDate(java.sql.Timestamp crawlerDate) {
+    this.crawlerDate = crawlerDate;
+  }
+
+
+  public String  getOrderNum() {
+    return orderNum;
+  }
+
+  public void setOrderNum(String orderNum) {
+    this.orderNum = orderNum;
+  }
+
+  public String getAvgTime() {
+    return avgTime;
+  }
+
+  public void setAvgTime(String avgTime) {
+    this.avgTime = avgTime;
+  }
+
+  public String getAvgCost() {
+    return avgCost;
+  }
+
+  public void setAvgCost(String avgCost) {
+    this.avgCost = avgCost;
+  }
+
+
+  public String getMethod() {
+    return method;
+  }
+
+  public void setMethod(String method) {
+    this.method = method;
+  }
+
+
+  public String getPhone2() {
+    return phone2;
+  }
+
+  public void setPhone2(String phone2) {
+    this.phone2 = phone2;
+  }
+
+
+  public long getIsNew() {
+    return isNew;
+  }
+
+  public void setIsNew(long isNew) {
+    this.isNew = isNew;
+  }
+
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+
+  public String getScore() {
+    return score;
+  }
+
+  public void setScore(String score) {
+    this.score = score;
+  }
+
+
+  public String getDistance() {
+    return distance;
+  }
+
+  public void setDistance(String distance) {
+    this.distance = distance;
+  }
+
+}
